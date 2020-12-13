8ab06766512ee546abb0b74df0e774736bd3f0ea
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2020-12-13 17:58:36 +0700

    [EDIT] deleted db.py, changed config.py

--- models/config.py
+++ models/config.py
@@ -1,4 +1,4 @@
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = ''
-DB_NAME = 'ftti_vote'
+DB_HOST = 'us-cdbr-east-02.cleardb.com'
+DB_USER = 'be8879fa8c7e16'
+DB_PASSWORD = '707a5465'
+DB_NAME = 'heroku_7926aa433849be4'
